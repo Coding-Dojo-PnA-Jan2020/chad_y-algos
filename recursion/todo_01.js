4381581309fcb4af75f621d44c5886919cf5d1a3
Chad Yantorno <tupacalypse187@gmail.com>
2020-01-25 10:26:34 -0800

    todo_01 completed

--- recursion/todo_01.js
+++ recursion/todo_01.js
@@ -18,7 +18,7 @@ console.log(recSigma(5));
 // Given num, return the product of ints from 1 up to num. If less than zero, treat as zero. If not an integer, truncate. Experts tell us 0! is 1. rFact(3) = 6 (1*2*3). Also, rFact(6.5) = 720 (1*2*3*4*5*6).
 function recFactorial(val) {
     val = Math.floor(val);
-    if (val == 1) {
+    if (val <= 1) {
         return 1;
     }
     return (val * recFactorial(val - 1));
