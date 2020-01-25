21acb73ada423cb42077bd056df495d83538f2b0
Chad Yantorno <tupacalypse187@gmail.com>
2020-01-25 11:30:05 -0800

    todo_03 complete

--- recursion/todo_03.js
+++ recursion/todo_03.js
@@ -32,3 +32,29 @@ console.log(rBinarySearch([-5,2,4,9,12],5));
 // gcf(a,b) == gcf(a-b,b), if a>b;
 // gcf(a,b) == gcf(a,b-a), if b>a.
 // Second: rework facts #2 and #3 to reduce stack consumption and expand rGCF’s reach. You should  be able to compute rGCF(123456,987654).
+function rGCF(num1, num2) {
+    if (num1 == num2) {
+        return num1;
+    }
+    if (num1 > num2) {
+        return rGCF(num1-num2, num2);
+    }
+    if (num2 > num1) {
+        return rGCF(num1, num2-num1);
+    }
+}
+
+console.log(rGCF(1800, 1020));
+// console.log(rGCF(123456, 987654)); // Stack exceeded
+
+function rGCF2(num1, num2) {
+    if (num2 == 0) {
+        return num1;
+    }
+    else {
+        return rGCF2(num2, (num1 % num2));
+    }
+}
+
+console.log(rGCF2(1800, 1020));
+console.log(rGCF2(123456, 987654));
