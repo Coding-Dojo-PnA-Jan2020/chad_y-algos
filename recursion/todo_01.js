027a18a8844576c332217a785edbca3f316e80b1
Chad Yantorno <tupacalypse187@gmail.com>
2020-01-25 10:13:52 -0800

    1 done

--- recursion/todo_01.js
+++ recursion/todo_01.js
@@ -2,6 +2,15 @@
 // Recursive Sigma
 // Write a recursive function that given a number returns the sum of integers from 1 to that number.
 // Example: rSigma(5) = 15 (1+2+3+4+5); rSigma(2.5) = 3 (1+2); rSigma(-1) = 0.
+function recSigma(val) {
+    if (val === 0) {
+        return 0;
+    }
+    // console.log(val);
+    return val + recSigma(val - 1);
+}
+
+console.log(recSigma(5));
 
 
 
